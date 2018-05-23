5e3bca9ce5ac689a5a2b8a42803aded372700aa7
yuzhe <thecourageyu@gmail.com>
2018-05-23 16:42:41 +0800

    git initialized on 20180522.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Output("Program start!");
+            // Output(args);
+            var webHost = BuildWebHost(args);
+            webHost.Run();
+            // BuildWebHost(args).Run();
+        }
+
+/*         public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build(); */
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            Output("Create WebHost Builder");
+            var webHostBuilder = WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    Output("webHostBuilder.ConfigureServices - Called");
+                })
+                .Configure(app =>
+                {
+                    Output("webHostBuilder.Configure - Called");
+                })
+                .UseStartup<Startup>();
+
+            Output("Build WebHost");
+            var webHost = webHostBuilder.Build();
+
+            return webHost;
+        }        
+
+        public static void Output(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}] {message}");
+        }
+    }
+}
