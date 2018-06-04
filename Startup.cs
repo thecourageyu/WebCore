00b7f6ceec90599c205acd514ce3d7603747600a
yuzhe <thecourageyu@gmail.com>
2018-06-04 08:35:00 +0800

    Routine push on 20180604.

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebCore
@@ -42,12 +43,39 @@ namespace WebCore
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
             Program.Output("Startup.ConfigureServices - Called");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IApplicationLifetime appLifetime)
         {
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            var defaultRouteHandler = new RouteHandler(context =>
+            {
+                var routeValues = context.GetRouteData().Values;
+                return context.Response.WriteAsync($"Route values: {string.Join(", ", routeValues)}");
+            });
+
+            var routeBuilder = new RouteBuilder(app, defaultRouteHandler);
+            routeBuilder.MapRoute("default", "{first:regex(^(default|home)$)}/{second?}");
+
+            routeBuilder.MapGet("user/{name}", context => {
+                var name = context.GetRouteValue("name");
+                return context.Response.WriteAsync($"Get user. name: {name}");
+            });
+
+            routeBuilder.MapPost("user/{name}", context => {
+                var name = context.GetRouteValue("name");
+                return context.Response.WriteAsync($"Create user. name: {name}");
+            });
+
+            var routes = routeBuilder.Build();
+            app.UseRouter(routes);
+
             appLifetime.ApplicationStarted.Register(() =>
             {
                 Program.Output("ApplicationLifetime - Started");
@@ -70,13 +98,13 @@ namespace WebCore
             });
 
             // For trigger stop WebHost
-            var thread = new Thread(new ThreadStart(() =>
+/*             var thread = new Thread(new ThreadStart(() =>
             {
                 Thread.Sleep(5 * 1000);
                 Program.Output("Trigger stop WebHost");
                 appLifetime.StopApplication();
             }));
-            thread.Start();
+            thread.Start(); */
 
             Program.Output("Startup.Configure - Called");
         }
